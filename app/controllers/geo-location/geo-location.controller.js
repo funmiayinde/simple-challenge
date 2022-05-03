59e61af148a5ecbe665058eeffc24484c9ce9ceb
funmiayinde <funmiayinde11@gmail.com>
2022-05-03 16:20:34 +0100

    do validation

--- app/controllers/geo-location/geo-location.controller.js
+++ app/controllers/geo-location/geo-location.controller.js
@@ -1,10 +1,11 @@
 import _ from "lodash";
 import AppError from "../../utils/app-error";
 import AppResponse from "../../utils/app-response";
-import { OK } from "../../utils/codes";
+import { BAD_REQUEST, OK } from "../../utils/codes";
 import GeoLocationProcessor from "./geo-location.processor";
 import GeoLocationValidation from "./geo-location.validation";
-import lang from '../../lang'
+import lang from '../../lang';
+import Validator from "validatorjs";
 
 /**
  * @class {GeoLocationController}
@@ -19,10 +20,15 @@ class GeoLocationController {
      */
     static async fetchGeoLocation(req, res, next) {
         try {
-            const validate = GeoLocationValidation.validation(req.query);
-            console.log('validate:', validate);
-            if (!validate.passed) {
-                return next(new AppError(lang.get('error').inputs, BAD_REQUEST, validate.errors));
+            const rules = {
+                min_lat: 'required',
+                min_lon: 'required',
+                max_lat: 'required',
+                max_lon: 'required',
+            };
+            const validator = new Validator(req.query, rules);
+            if (!validator.passes()) {
+                return next(new AppError(lang.get('error').inputs, BAD_REQUEST, validator.errors.all()));
             }
             const data = await GeoLocationProcessor.fetchGeoLocation(req.query);
             return res.status(OK).json(AppResponse.format(AppResponse.getSuccessMeta(), data));
