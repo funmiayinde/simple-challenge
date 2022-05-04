336e0c57f1cd96779d01318967277c7fbdfa4d9e
funmiayinde <funmiayinde11@gmail.com>
2022-05-04 02:50:43 +0100

    update unit test

--- __test__/geo-location/geo-location.controller.spec.js
+++ __test__/geo-location/geo-location.controller.spec.js
@@ -4,7 +4,7 @@ import { OPEN_STREET_URL } from '../../app/utils/helper';
 import axios from '../../__mock__/axios';
 import { TEST_GEO_LOCATION_URL } from '../_config/routes';
 import { sampleData } from '../_seed/geo-location.seed';
-import { NOT_FOUND, OK } from '../../app/utils/codes';
+import { OK } from '../../app/utils/codes';
 
 
 afterAll(() => {
@@ -24,8 +24,9 @@ test('test fetching geo-locations from openstreetmap ', async () => {
   expect(response.body).toBeInstanceOf(Object);
   expect(response.body._meta).toBeInstanceOf(Object);
   expect(response.body._meta).toHaveProperty('status_code');
-  expect(response.body._meta.error).toBeInstanceOf(Object);
-  expect(response.body._meta.error).toHaveProperty('code');
-  expect(response.body._meta.error).toHaveProperty('message');
-  expect(response.status).toBe(NOT_FOUND);
+  expect(response.body.data).toBeInstanceOf(Object);
+  expect(response.body.data).toHaveProperty('type');
+  expect(response.body.data).toHaveProperty('features');
+  expect(response.body.data.features).toBeInstanceOf(Array);
+  expect(response.status).toBe(OK);
 });
